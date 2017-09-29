3b8c4833c706c249fcaaa4db902b72deef757eeb
Teraaret <raiiliiat@yandex.ru>
2017-09-29 17:09:46 +0500

    2

--- src/index.js
+++ src/index.js
@@ -1,40 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Footer from './Footer';
 import './index.css';
+import Footer from './Footer';
 
 // ***
 
 var data = {
-    title: "React App"
+    title: "React App",
+    users: [
+        {id: 1, name: 'John'},
+        {id: 2, name: 'Kaeli'},
+        {id: 3, name: 'Orni'}
+    ]
 }
 
 var components = {
     title: <h1 className="title">{ data.title }</h1>,
     header: <h1 className="header">The header</h1>,
-    paragraph: <p className="paragraph">"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores minus saepe dicta dolorum rem veniam nostrum nulla dolorem mollitia repellendus sit corporis id illum est consequuntur aut perspiciatis, voluptates natus maxime, quaerat itaque architecto deserunt ab odio. Cupiditate, esse nobis"</p>
+    paragraph: <p className="paragraph">"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores minus saepe dicta dolorum rem veniam nostrum nulla dolorem mollitia repellendus sit corporis id illum est consequuntur aut perspiciatis, voluptates natus maxime, quaerat itaque architecto deserunt ab odio. Cupiditate, esse nobis"</p>,
+    list: <ul>{data.users.map(user => <li key={user.id}>{user.name}</li>)}</ul>
     }
 
-function DeleteUserLink() {
-    function onClick(e) 
+class Tumbler extends React.Component {
+    constructor(props)
     {
-            e.preventDefault();
-            console.log('Пользователь был удален.');
+        super(props);
+        this.state = {isActive: true};
+        this.onOff = this.onOff.bind(this);
     }
-    return ( <a href="#nothing" onClick={onClick}>Удалить пользователя</a> );
+    
+    onOff()
+    {
+        this.setState({ isActive: !this.state.isActive });
+    }
+    
+    render() {
+        let isActive = this.state.isActive;
+        return (<div>
+            {isActive && <h2>Включено</h2>}
+            {!isActive && <h2>Выключено</h2>}
+            <h2>{ isActive ? "Включено" : "Выключено" }</h2>
+            <button onClick={this.onOff}>on off</button>
+        </div>);  
+    }
 }
-
-//function Timer() 
-//{
-//    var now = new Date();
-//    var end = new Date(2017, now.getMonth(), now.getDate(), 18, 0, 0, 0);
-//    var diff = (end.getTime() - now.getTime()) / 1000 / 60 / 60;
-//    diff = diff.toFixed(5);
-//    
-//    return <p className="timer" id="timer">{ diff }</p>;
-//}
             
-class TimerNew extends React.Component {
+class Timer extends React.Component {
     constructor(props)
     {
         super(props);
@@ -60,40 +71,42 @@ class TimerNew extends React.Component {
     }
 }
 
-class Conditioner extends React.Component {
+class LoginForm extends React.Component {
     constructor(props) {
-        super(props);
-        this.state = {temperature: 0};
+      super(props);
+      this.state = {login: '', password: ''};
 
-        // Привязка необходима, чтобы сделать this доступным в коллбэке
-        this.onIncrease = this.onIncrease.bind(this);
-        this.onDecrease = this.onDecrease.bind(this);
+      this.onLoginChange = this.onLoginChange.bind(this);
+      this.onPasswordChange = this.onPasswordChange.bind(this);
+      this.onSubmit = this.onSubmit.bind(this);
     }
 
-    onIncrease()
-    {
-        this.setState(prevState => 
-        ({ temperature: prevState.temperature + 1 })
-                     )
+    onSubmit(){
+      alert(`${this.state.login}, добро пожаловать!`);
+      window.event.preventDefault();
     }
 
-    onDecrease()
-    {
-        this.setState(prevState => 
-        ({ temperature: prevState.temperature - 1 })
-                   )
+    onPasswordChange(e){
+      this.setState({password: e.target.value});
+    }
+
+    onLoginChange(e) {
+      this.setState({login: e.target.value});
     }
 
     render() {
-        return (<div>
-            <h2>Текущая температура: {this.state.temperature}</h2>
-            <button onClick={this.onDecrease}>-</button>
-            <button onClick={this.onIncrease}>+</button>
-        </div>);
+      return (
+        <form onSubmit={this.onSubmit}>
+          <p><label> Логин: <input type="text" name="login" value={this.state.login}
+                           onChange={this.onLoginChange}/></label></p>
+          <p><label> Пароль: <input type="password" name="password" value={this.state.password}
+                            onChange={this.onPasswordChange}/></label></p>
+          <p><input type="submit" value="Submit" /></p>
+        </form>
+      );
     }
   }
 
-            
 ////////////////////
             
 //function rendering()
@@ -104,12 +117,20 @@ class Conditioner extends React.Component {
             <hr/>
             { components.header }
             { components.paragraph }
-            < TimerNew />
-            < DeleteUserLink />
-            < Conditioner />
+            { components.list }
+            < Timer />
+            < Tumbler />
+            <LoginForm />
             <Footer />
         </div>,
         document.getElementById('root')
     );
 //}
 //setInterval(rendering, 10);
+        
+var firstname = "OLEg";
+let str = `
+Здравствуйте ${firstname}.
+Мы в ${new Date().getFullYear()} году
+`;
+console.log(str);
